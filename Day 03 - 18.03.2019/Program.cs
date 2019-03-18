e0e5f8fa1553d4894b18da78daae7c2afeb5445c
shadibdair <43513994+shadibdair@users.noreply.github.com>
2019-03-19 00:00:05 +0200

    Add files via upload

---
+++ Day 03 - 18.03.2019/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> StudentList = new List<Student>();
+            StudentList.Add(new Student("Shadi", 98, 20));
+            StudentList.Add(new Student("Naor", 90, 24));
+            StudentList.Add(new Student("Ofek", 85, 23));
+            StudentList.Add(new Student("Bob", 60, 24));
+            StudentList.Add(new Student("Alice", 99, 30));
+
+            Console.WriteLine("**Welcome**");
+
+            // --------------------------------------------------------------------------
+            // Way 1
+
+            var NAMEE = from element in StudentList
+                          orderby element.Name
+                          select element;
+            Console.WriteLine();
+            Console.WriteLine("Sorted By Name: ");
+            // sorted Name
+            foreach (var value in NAMEE)
+            {
+              
+                Console.WriteLine(value.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine("_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+            var AGEE = from element in StudentList
+                          orderby element.Age
+                          select element;
+            Console.WriteLine("Sorted By Age: ");
+            // sorted Age
+            foreach (var value in AGEE)
+            {
+
+                Console.WriteLine(value.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine("_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+            var GRADEE = from element in StudentList
+                          orderby element.Grade
+                          select element;
+            Console.WriteLine("Sorted By Grade: ");
+            // sorted Grade
+            foreach (var value in GRADEE)
+            {
+
+                Console.WriteLine(value.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine("_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+
+            // --------------------------------------------------------------------------
+            // Way 2
+
+            Console.WriteLine("Sorted By Age: ");
+            StudentList.Sort((s1, s2) => s2.Age - s1.Age);
+            StudentList.ForEach(s => Console.WriteLine(s.ToString()));
+
+            Console.WriteLine("Sorted By Grade: ");
+            StudentList.Sort((s1, s2) => s2.Grade - s1.Grade);
+            StudentList.ForEach(s => Console.WriteLine(s.ToString()));
+
+            Console.WriteLine("Sorted By Name: ");
+            StudentList.Sort((s1, s2) => s1.Name.CompareTo(s2.Name));
+            StudentList.ForEach(s => Console.WriteLine(s.ToString()));
+
+            // --------------------------------------------------------------------------
+
+            // var vs dynamic ??
+        }
+    }
+}
---
+++ Day 03 - 18.03.2019/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp43
+{
+    class Student
+    {
+        private int grade;
+
+        public int Grade
+        {
+            get { return grade; }
+            set {
+                if(grade>= 0 && grade<=100)
+                {
+                    grade = value;
+                } 
+            }
+        }
+
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set {
+                if (age >= 18 && age <= 120)
+                {
+                    age = value;
+                }
+            }
+        }
+
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Student(string Name, int Grade,int Age)
+        {
+            name = Name;
+            grade = Grade;
+            age = Age;
+        }
+
+        public string ToString()
+        {
+            return $"Name: {name} Age: {age} Grade: {grade}"; 
+        }
+
+    }
+}
