08472b190be3ea14d6a1d4b2d2f5aec6cb376899
shadibdair <43513994+shadibdair@users.noreply.github.com>
2019-03-19 00:25:35 +0200

    Add files via upload

---
+++ Day 03 - 18.03.2019/02_var VS dynamic/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             (Dynamic):
+             Store any type of value in the dynamic data type variable created using 
+             dynamic keyword. Type checking for these types of variables
+             takes place at run-time. 
+             Dynamic are dynamically typed variables.
+
+            The dynamic types are similar to object types except that 
+            type checking for object type variables takes place at compile time,
+            whereas that for the dynamic type variables takes place at runtime.
+             */
+
+            /*
+             (Var):
+             The "var" keyword initializes variables with var support. 
+             Just assign whatever value you want for the variable,
+             integer, string, float, etc. It is a statically typed variable.
+             */
+
+
+            var x = "string1"; // early bounded
+            int len = x.Length;
+            Console.WriteLine(len); // 7
+
+            dynamic y = "string1"; // late bounded
+            int len1 = y.length; // RuntimeBinder.RuntimeBinderException: 'string'
+
+
+
+
+            
+        }
+    }
+}
