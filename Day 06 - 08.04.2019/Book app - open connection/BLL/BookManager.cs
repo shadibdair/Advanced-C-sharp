cb91270d114d9635426787b494db56a476c570c5
shadibdair <43513994+shadibdair@users.noreply.github.com>
2019-04-13 01:55:39 +0300

    connect to Db in Csharp

---
+++ Day 06 - 08.04.2019/Book app - open connection/BLL/BookManager.cs
@@ -0,0 +1,25 @@
+﻿using BOL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+
+    public class BookManager
+    {
+
+        //the UIL will call this function and send a BOL object
+        //the BLL will add the object t the EF
+        //and save the changes to the DAL
+        public static void InsertBookByDbSet(BookModel newBook)
+        {
+            DbManager.ExecDb($"insert into dbo.Books(BookName, BookPrice, AuthorID) values ('{newBook.BookName}',{newBook.BookPrice},{newBook.AuthorID})");
+        }
+
+    }
+}
+
