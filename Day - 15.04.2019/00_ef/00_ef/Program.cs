f9c494724b0b9f39bcdcc89f2defe525b2389999
shadibdair <43513994+shadibdair@users.noreply.github.com>
2019-04-15 15:07:40 +0300

    Add files via upload

---
+++ Day - 15.04.2019/00_ef/00_ef/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_ef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (NorthwindEntities ef = new NorthwindEntities())
+                {
+                    foreach (Category item in ef.Categories)
+                    {
+                        Console.WriteLine(item.CategoryName);
+                    }
+
+                    Category change = ef.Categories.FirstOrDefault(x => x.CategoryName.StartsWith("B"));
+                    Console.WriteLine($"we selected: {change.CategoryName}");
+
+                    change.CategoryName = "NewCategory";
+                    Console.WriteLine($"we changed to: {change.CategoryName}");
+
+                    ef.SaveChanges();
+
+                }
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Error...");
+            }
+            }
+    }
+}
