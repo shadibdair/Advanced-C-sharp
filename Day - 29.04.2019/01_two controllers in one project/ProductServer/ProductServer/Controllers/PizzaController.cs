a0d2ce050f53e39d6785e814b9b26a9cae0e1230
shadibdair <43513994+shadibdair@users.noreply.github.com>
2019-04-29 18:48:49 -0400

    Add files via upload

---
+++ Day - 29.04.2019/01_two controllers in one project/ProductServer/ProductServer/Controllers/PizzaController.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Web.Http;
+
+namespace ProductServer.Controllers
+{
+    public class PizzaController : ApiController
+    {
+
+        public string Get()
+        {
+            return "pizza get";
+        }
+
+
+        public string Delete()
+        {
+            return "pizza delete";
+        }
+
+
+    }
+}
---
+++ Day - 29.04.2019/01_two controllers in one project/ProductServer/ProductServer/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace ProductServer.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public string Get()
+        {
+            return "product get";
+        }
+
+
+        public string Delete()
+        {
+            return "product delete";
+        }
+
+    }
+}
