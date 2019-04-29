3d09bf90ae449593f391b8c9c30577202849fa30
shadibdair <43513994+shadibdair@users.noreply.github.com>
2019-04-29 18:44:47 -0400

    Add files via upload

---
+++ Day - 29.04.2019/00_basic controller in web api/ProductServer/ProductServer/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ProductServer.Controllers
+{
+    public class ProductController : ApiController
+    {
+
+        // GET: api/Product
+        public string Get()
+        {
+            return "get req";
+        }
+
+        // POST: api/Product
+        public string Post()
+        {
+            return "post req";
+        }
+
+        // PUT: api/Product
+        public string Put()
+        {
+            return "put req";
+        }
+
+        // DELETE: api/Product
+        public string Delete()
+        {
+            return "delete req";
+        }
+    }
+}
