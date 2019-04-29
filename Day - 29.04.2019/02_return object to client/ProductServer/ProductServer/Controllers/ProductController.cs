5141fda8384de41d86036736810aceaa22f068ed
shadibdair <43513994+shadibdair@users.noreply.github.com>
2019-04-29 18:52:06 -0400

    Add files via upload

---
+++ Day - 29.04.2019/02_return object to client/ProductServer/ProductServer/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using ProductServer.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ProductServer.Controllers
+{
+    public class ProductController : ApiController
+    {
+
+        static List<Product> arrProducts = new List<Product> {
+             new Product {id=1,name="bamba", price=5 },
+             new Product {id=2,name="bread", price=5 },
+             new Product {id=3,name="milk", price=6 },
+             new Product {id=4,name="bisli", price=7 },
+             new Product {id=5,name="banana", price=12 }
+         };
+
+        public List<Product> Get()
+        {
+            return arrProducts;
+        }
+
+
+        public Product Get(int id)
+        {
+            return arrProducts.FirstOrDefault(x=>x.id==id);
+        }
+
+    }
+}
---
+++ Day - 29.04.2019/02_return object to client/ProductServer/ProductServer/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ProductServer.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+}
